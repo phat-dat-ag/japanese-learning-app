0afe28f376ef5fec31d1a4c51a18b2c85c7e3993
phat-dat-ag <67agphatdat@gmail.com>
2026-06-28 15:21:29 +0700

    chore(backend): remove health sample code

--- backend/src/test/java/com/dat/health/HealthResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.dat.health;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class HealthResourceIT extends HealthResourceTest {
-    // Execute the same tests but in packaged mode.
-}
